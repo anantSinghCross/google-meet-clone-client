a63ce2a64beb22b63798f683fbb567f1b1492b2d
Anant Singh <38243636+anantSinghCross@users.noreply.github.com>
2025-07-19 14:05:02 +0530

    Adjust notification position to top right corner

--- src/components/Notifications.jsx
+++ src/components/Notifications.jsx
@@ -8,7 +8,7 @@ const Notifications = () => {
   return (
     <>
       {call.isReceivingCall && !callAccepted && (
-        <div className="fixed border-2 border-indigo-400 shadow-md shadow-indigo-100 bottom-4 right-4 flex gap-4 p-3 rounded-xl justify-center w-max items-center">
+        <div className="fixed border-2 border-indigo-400 shadow-md shadow-indigo-100 top-4 right-4 flex gap-4 p-3 rounded-xl justify-center w-max items-center">
           <BiSolidPhoneIncoming className=" fill-indigo-500" size={25}/>
           <h3> {call.name === "" ? "Unknown person" : call.name} is calling</h3>
           <button className="btn-blue-grad" onClick={answerCall}>
